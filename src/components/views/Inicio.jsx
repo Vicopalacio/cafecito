242f59e6032a25ba86e46e9f9ee5a311e4629b5f
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-10-18 00:22:56 -0300

    agregamos la funcion para mostrar en la pagina de inicio los productos cargados en administrador

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import { Container, Row } from "react-bootstrap";
+import { consultarApi } from "../helpers/queries";
 import CardProd from "./producto/CardProd";
+import { useEffect } from "react";
 
 const Inicio = () => {
-    return (
-        <Container className='mainSection'>
-            <h3 className='display-4 text-center mt-5'>Bienvenidos</h3>
-            <hr />
-            <Row>
-            <CardProd></CardProd>
-            <CardProd></CardProd>
-            <CardProd></CardProd>
-            </Row>
-        </Container>
-    );
+  const [verProd, setVerProd] = useState([]);
+
+  useEffect(() => {
+    consultarApi().then((laRespuesta) => {
+      setVerProd(laRespuesta);
+    });
+  }, []);
+
+  return (
+    <Container className="mainSection">
+      <h3 className="display-4 text-center mt-5">Bienvenidos</h3>
+      <hr />
+      <Row>
+        {
+        verProd.map((producto) => (
+          <CardProd
+            key={producto.id}
+            producto={producto}
+            verProd={verProd}
+          ></CardProd>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Inicio;
--- src/components/views/producto/CardProd.jsx
+++ src/components/views/producto/CardProd.jsx
@@ -1,19 +1,20 @@
 import { Card, Button, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const CardProd = () => {
+const CardProd = ({producto}) => {
+
   return (
     <>
       <Col md={3} className='my-2'>
         <Card>
           <Card.Img
             variant="top"
-            src="https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src={producto.imagen}
             className='px-2'
           />
           <Card.Body>
-            <Card.Title>Sandwich</Card.Title>
-            <Card.Text>Precio: $450.</Card.Text>
+            <Card.Title>{producto.nombreProducto}</Card.Title>
+            <Card.Text>Precio: ${producto.precio}.</Card.Text>
             <Button variant="danger" as={Link} to='/administrar/detalleprod'>Ver más</Button>
           </Card.Body>
         </Card>
